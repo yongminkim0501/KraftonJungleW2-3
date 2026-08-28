9ee8dc1bf34ebdac5e7caf02f4f3aff993ff4482
yongminkim0501 <yongmingim166@gmail.com>
2026-08-28 14:35:54 +0900

    solve-issue-05

--- week2/1. basic/05_recursion.py
+++ week2/1. basic/05_recursion.py
@@ -34,7 +34,9 @@ def factorial(n):
     """
     # TODO: base case를 작성하세요
     # n이 0이거나 1이면 1을 반환
+    if n == 0 or n == 1 : return 1
     pass
+    return n*factorial(n-1)
     
     # TODO: recursive case를 작성하세요
     pass
@@ -51,9 +53,12 @@ def fibonacci(n):
     """
     # TODO: base case를 작성하세요
     # n이 0이면 0, n이 1이면 1 반환
+    if n == 0 : return 0
+    if n == 1 : return 1
     pass
     
     # TODO: recursive case를 작성하세요
+    return fibonacci(n-1) + fibonacci(n-2)
     pass
 
 # 테스트 케이스
