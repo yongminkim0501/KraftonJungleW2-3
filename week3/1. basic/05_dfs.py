af300bf1254300adaa4f4cd5bd840d9f82ca700f
yongminkim0501 <yongmingim166@gmail.com>
2026-09-04 11:18:59 +0900

    solve: basic issue - 5

--- week3/1. basic/05_dfs.py
+++ week3/1. basic/05_dfs.py
@@ -29,29 +29,20 @@ DFS: [0, 1, 2, 3] (순서는 구현에 따라 다를 수 있음)
 - 방문 체크 필요
 - 깊이 우선으로 방문
 """
-
 def dfs(graph, start, visited=None):
-    """
-    깊이 우선 탐색 (재귀)
-    
-    Args:
-        graph: 그래프 딕셔너리
-        start: 현재 정점
-        visited: 방문 리스트
-    
-    Returns:
-        방문 순서 리스트
-    """
-    # TODO: visited가 None이면 초기화
-    pass
-    
-    # TODO: 현재 정점 방문
-    pass
-    
-    # TODO: 인접한 정점들에 대해 재귀
-    ## 방문하지 않은 정점이면 재귀 호출
-    pass
-    
+    is_visited = [False for _ in range(len(graph))]
+
+    visited = []
+
+    def dfs_iter(graph, start, visited):
+        visited.append(start)
+        is_visited[start] = True
+        for node in graph:
+            for edge in graph[node]:
+                if is_visited[edge] == False:
+                    dfs_iter(graph, edge, visited)
+
+    dfs_iter(graph, start, visited)
     return visited
 
 # 테스트 케이스
